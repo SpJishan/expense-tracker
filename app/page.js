60f7d176e5690611cbded46dc2ba5e8d5d82b3ac
Shafinul Pasha <jishan523@gmail.com>
2024-01-31 20:51:29 +0600

    Completing main layout , dynamically with tailwindCSS
    1. Setting state variables
    2. Creating Map for state variables to display some un-ordered list.
    3. Giving css property to the mapped list
    4. Tarnary logic if there is a list show total or leave empty string.

--- app/page.js
+++ app/page.js
@@ -1,6 +1,15 @@
-import Image from "next/image";
+"use client"
+import React, {useState, useEffect} from "react";
 
 export default function Home() {
+  // Setting state variables
+  const [items, setItems] = useState([
+    {name: 'Tea', price: 0.2 },
+    {name: 'Coffee', price: 0.2},
+    {name: 'Movie', price: 0.6},
+    {name: 'candy', price: 7.75},
+  ]);
+  const [total, setTotal] = useState(0);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between sm:p-24 p-1">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm ">
@@ -11,6 +20,27 @@ export default function Home() {
           <input className="col-span-2 border mx-3 p-3" type="number" placeholder='Enter $' />
           <button className="text-white bg-slate-900 hover:bg-slate-900 p-3 text-xl" type="submit">+</button>
         </form>
+        <ul>
+        {/* Creating Map for state variables to display some un-ordered list. */}
+
+          {items.map((item, id)=>(
+            <li key={id} className="text-white my-4 w-full flex justify-between bg-slate-900">
+              <div className="p-4 w-full flex justify-between ">
+                <span className="capitalize">{item.name}</span>
+                <span>$ {item.price}</span>
+              </div>
+              <button className="ml-8 p-4 border-l-2 border-slate-950 hover:bg-slate-950 w-16">X</button>
+            </li>
+          ))}
+        </ul>
+        {/* Tarnary logic if there is a list show total or leave empty string. */}
+        
+        {items.length < 1 ? (''):(
+          <div className="flex justify-between p-3">
+            <span>Total </span>
+            <span>$ {total}</span>
+          </div>
+        )}
       </div>
       </div>
     </main>
